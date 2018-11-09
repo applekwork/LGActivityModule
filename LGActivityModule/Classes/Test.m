cfc03e7fa18f4447091be14ceded6b8a2958ff44
郭立军 <wxguolj@yutong.com>
2018-11-09 14:50:03 +0800

    df

--- LGActivityModule/Classes/Test.m
+++ LGActivityModule/Classes/Test.m
@@ -68,7 +68,75 @@ typedef void(^LGWebViewBridgeCallback)(NSString *result, BOOL completed);
 //        [self returnResult:0 dic:info callback:callback];
     }
 }
-
+#pragma mark - 关闭监听网络
+- (void)stopNetWorkStatus:(NSDictionary *)args callback:(YTWebViewBridgeCallback)callback {
+    if (callback) {
+        [self.reachabilityManager stopMonitoring];
+//        [self returnResult:0 callback:callback];
+    }
+}
+#pragma mark - 开始监听加速度计状态
+- (void)startAccelerometer:(NSDictionary *)args callback:(YTWebViewBridgeCallback)callback {
+    if (callback) {
+        if (![self.motionManager isAccelerometerAvailable]) {
+            //加速计不可用
+            NSLog(@"Accelerometer is not Available");
+            return;
+        }
+        [self.motionManager startAccelerometerUpdatesToQueue:[[NSOperationQueue alloc] init] withHandler:^(CMAccelerometerData * _Nullable accelerometerData, NSError * _Nullable error) {
+            //获取加速度
+            CMAcceleration acceleration = accelerometerData.acceleration;
+            NSLog(@"加速度 == x:%f, y:%f, z:%f", fabs(acceleration.x), fabs(acceleration.y), fabs(acceleration.z));
+            //值越大说明摇动的幅度越大
+            double num = 1.5f;
+            if (fabs(acceleration.x) > num || fabs(acceleration.y) > num ||fabs(acceleration.z) > num) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    NSMutableDictionary *info = [NSMutableDictionary dictionaryWithCapacity:0];
+                    [info setValue:@(acceleration.x) forKey:@"x"];
+                    [info setValue:@(acceleration.y) forKey:@"y"];
+                    [info setValue:@(acceleration.z) forKey:@"z"];
+//                    [self returnResult:0 dic:info callback:callback];
+                });
+            }
+        }];
+    }
+}
+#pragma mark - 停止监听加速度计状态
+- (void)stopAccelerometer:(NSDictionary *)args callback:(YTWebViewBridgeCallback)callback {
+    if (callback) {
+        if ([self.motionManager isAccelerometerActive]) {
+            [self.motionManager stopAccelerometerUpdates];
+//            [self returnResult:0 callback:callback];
+        }
+    }
+}
+#pragma mark - 设置系统剪切板内容
+- (void)setClipboardData:(NSDictionary *)args callback:(YTWebViewBridgeCallback)callback {
+    if (callback) {
+        NSString *pasterStr = [args valueForKey:@"data"];
+        if (pasterStr && pasterStr.length > 0) {
+            UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+            pasteboard.string = pasterStr;
+//            [self returnResult:0 callback:callback];
+        } else {
+//            [self returnResult:1 callback:callback];
+        }
+    }
+}
+#pragma mark - 获取系统剪切板内容
+- (void)getClipboardData:(NSDictionary *)args callback:(YTWebViewBridgeCallback)callback {
+    if (callback) {
+        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+        NSString *pasterStr = pasteboard.string;
+        if (pasterStr && pasterStr.length > 0) {
+            NSMutableDictionary *info = [NSMutableDictionary dictionaryWithCapacity:0];
+            [info setValue:pasterStr forKey:@"content"];
+//            [self returnResult:0 dic:info callback:callback];
+        } else {
+//            [self returnResult:1 callback:callback];
+        }
+    }
+}
 - (AFNetworkReachabilityManager *)reachabilityManager {
     if (!_reachabilityManager) {
         _reachabilityManager = [AFNetworkReachabilityManager managerForDomain:@"www.baidu.com"];
